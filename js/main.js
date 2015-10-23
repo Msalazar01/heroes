70a49a907ff6e820a5a03f75236412e32ec8ed24
Graphic <graphic@SOD-SJH-516-23M.local>
2015-10-23 12:47:11 -0400

    Last weeks in class solution

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 
 $('.hero').filter(':first').addClass('shown');
 
-$('h1').text("Batman");
-
 $('button.next').on('click', function(e) { 
 	
-	$('.shown').next().addClass('shown');
-	$('.shown').filter(':first').removeClass('shown');
+	if ( $('.shown').is(':last-child') ) {
+		 $('.shown').removeClass('shown');
+		 $('.hero').filter(':first').addClass('shown');
+
+	}
+	else {
+		$('.shown').removeClass('shown').next().addClass('shown');
+	}
+
+	var caption = $('.shown').attr('title');
+	$('.name').text(caption).show().FadeOut(1000);
 
 });
